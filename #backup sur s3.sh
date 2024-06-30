cbcf74191a3b9839fb89ba39a721211f385830de
Mastaldie <139794835+Mastaldie@users.noreply.github.com>
2024-06-30 20:02:28 +0100

    Add files via upload

---
+++ #backup sur s3.sh
@@ -0,0 +1,22 @@
+#backup sur s3
+#crée un dossier 
+mkdir file_name
+#crée un role et un S3 puis donner les autorisations IAM fullacces S3,puis lier IAM a l'instance EC2
+#zipper le dossier avec un script shell et la sauvegarder sur s3
+
+
+#!/bin/bash
+
+# Chemin local vers votre dossier à sauvegarder
+SOURCE="/home/ec2-user/amisi"
+
+# Nom de votre compartiment S3
+BUCKET_NAME="mondisque"
+
+# Créez une archive tar du dossier
+tar -cvf backup.tar $SOURCE
+
+# Chargez l'archive dans Amazon S3
+aws s3 cp backup.tar s3://mondisque/
+
+
